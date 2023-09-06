5752cd2bdcdb8cd96fa413d4e153ed75c23c17a3
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-09-06 16:58:40 -0400

    Update nguzzone_vacuum.py

--- nguzzone_vacuum.py
+++ nguzzone_vacuum.py
@@ -7,12 +7,25 @@ Created on Tue Sep  5 17:49:24 2023
 
 from vacuum import VacuumAgent
 
-class nguzzoneVacuumAgent(VacuumAgent):
-    
+class NguzzoneVacuumAgent(VacuumAgent):
+
+    movements = ['Left','Right','Up', 'Down']
     def _init_(self):
         super().__init__()
         #any initiliztion you want to do here
+        self.last_move = ""
+        self.past_choice = ""
         
-        def program(self, percept):
+    def program(self, percept):
             # your amazing Ai vacuum cleaner code goes here
+            
+            print(percept[0], percept[1])
+            #if the square is clean and we haven't hit anything then continue moving as you were going
+            #if this is our first action in the sequence then pick a random direction and move in it
+            if(percept[0] == 'Clean' and percept[1] == 'None'):
+                if self.past_choice in movements:
+                    return self.past_choice
+                
+                elif self.past_choice == 'Suck':
+                    return self.last_move
             return 'NoOp'
