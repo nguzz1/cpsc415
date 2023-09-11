5341211394dce47a839fdd5c6a297dc130e0029d
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-09-11 19:53:59 -0400

    bug fix

--- nguzzone_vacuum.py
+++ nguzzone_vacuum.py
@@ -39,22 +39,22 @@ class NguzzoneVacuumAgent(VacuumAgent):
                 if percept[1] == 'Bump':
                     #bumpDetected = 0 means that a bump has been detected above. 
                     if self.last_move == 'Up' and self.bumpDetected == 0:
-                        bumpDetected = 1
+                        self.bumpDetected = 1
                         self.last_move = 'Left'
                         self.past_choice = 'Left'
                         return 'Left'
                     elif self.last_move == 'Left' and self.bumpDetected == 1:
-                        bumpDetected = 2
+                        self.bumpDetected = 2
                         self.last_move = 'Down'
                         self.past_choice = 'Down'
                         return'Down'
                     elif self.last_move == 'Down' and self.bumpDetected == 2:
-                        bumpDetected = 3
+                        self.bumpDetected = 3
                         self.last_move = 'Right'
                         self.past_choice = 'Right'
                         return 'Right'
                     elif self.last_move == 'Right' and self.bumpDetected == 3: 
-                        bumpDetected = 4
+                        self.bumpDetected = 4
                         self.last_move = 'Up'
                         self.past_choice = 'Up'
                         return 'Up'
