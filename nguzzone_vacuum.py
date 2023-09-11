266fbb4f59b884025a8f16c938f3ae86ce3c4184
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-09-11 08:45:50 -0400

    Update nguzzone_vacuum.py

--- nguzzone_vacuum.py
+++ nguzzone_vacuum.py
@@ -6,6 +6,7 @@ Created on Tue Sep  5 17:49:24 2023
 """
 
 from vacuum import VacuumAgent
+import random
 
 class NguzzoneVacuumAgent(VacuumAgent):
 
@@ -23,9 +24,25 @@ class NguzzoneVacuumAgent(VacuumAgent):
             #if the square is clean and we haven't hit anything then continue moving as you were going
             #if this is our first action in the sequence then pick a random direction and move in it
             if(percept[0] == 'Clean' and percept[1] == 'None'):
-                if self.past_choice in movements:
+                if self.past_choice in NguzzoneVacuumAgent.movements:
                     return self.past_choice
                 
-                elif self.past_choice == 'Suck':
+                elif self.past_choice == 'Suck' and self.lastmove != "":
                     return self.last_move
+                elif self.past_choice == 'NoOp' or self.lastmove == "":
+                    r = random.randint(0, 3)
+                    rmove = NguzzoneVacuumAgent.movements[r]
+                    self.lastmove = rmove
+                    return rmove
+            elif (percept[0] == 'Clean' and percept[1] == 'Bump'):
+                if self.last_move == 'Left':
+                    return 'Down'
+                elif self.last_move == 'Down':
+                    return 'Right'
+                elif self.last_move == 'Right':
+                    return 'Up'
+                elif self.last_move == 'Up':
+                    return 'Left'
+            elif (percept[0] == 'Dirty'):
+                return 'Suck'
             return 'NoOp'
