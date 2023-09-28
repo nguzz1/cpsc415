5ea0187a587998c735e4a3fb3b8b63f79d858fe2
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-09-28 15:40:49 -0400

    base main/cmd line

--- nguzzone_nqueens.py
+++ nguzzone_nqueens.py
@@ -17,9 +17,24 @@ def checkQueens(board):
 
 def getColumn(board):
     return None
-        
+
+#maybe do this idk
+def printBoard(board):
+    return None
+    
 def viewBoard(board):    
     return None
 
 def main():
+    if len(sys.argv) != 2:
+        print("Error: "'To use file type "nguzzone_nqueens.py n" where n is an integer')
+        sys.exit(1)
+        
+    n = int(sys.argv[1])
+    board = [random.randint(0, n-1) for i in range(n)]
+    goal = False
+    
+    while goal == False:
+        #check for solution. Call hill climb or checkQueens here.
+                
     return None 
