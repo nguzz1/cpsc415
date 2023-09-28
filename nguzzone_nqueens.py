9abaa65ee328f29a97fe87ce8688e3846fadde0d
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-09-28 15:26:02 -0400

    initial layout/commit
    had to start later than planned due to other classes and exams :(

---
+++ nguzzone_nqueens.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 15:04:00 2023
+
+@author: nicol
+"""
+
+import sys
+import random
+board = []
+
+def hillClimb(board):
+    return None
+
+def checkQueens(board):
+    return None
+
+def getColumn(board):
+    return None
+        
+def viewBoard(board):    
+    return None
+
+def main():
+    return None 
