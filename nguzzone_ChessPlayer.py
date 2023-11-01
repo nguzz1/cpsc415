d2e5e5211edeec75f7b7a7a5e7a37deb08f412de
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-10-31 21:55:44 -0400

    AlphaBeta
    Ran into a bug, reinstalling files

--- nguzzone_ChessPlayer.py
+++ nguzzone_ChessPlayer.py
@@ -16,10 +16,41 @@ class nguzzone_ChessPlayer(ChessPlayer):
 
 
     def get_move(self, your_remaining_time, opp_remaining_time, prog_stuff):
-        move = self.findBestMove(self.board)
+        move = self.findBestMove(self.board, your_remaining_time, opp_remaining_time,)
         #print(move)
         return move
         
+    
+    def alphaBeta(self, board, depth, alpha, beta, maxPlayer):
+        if depth == 0 or board.is_king_in_checkmate(self.color):
+            return self.evaluateBoard(board, depth)
+        
+        if maxPlayer:
+            maxEval = float(-99999)
+            for move in board.get_all_available_legal_moves(self.color):
+                newBoard = deepcopy(board)
+                newBoard.make_move(*move)
+                eval = self.alphaBeta(newBoard, depth - 1, alpha, beta, False)
+                maxEval = max(maxEval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+            print(maxEval)
+            return maxEval
+        else:
+            minEval = float(99999)
+            for move in board.get_all_available_legal_moves('black' if self.color == 'white' else 'white'):
+                newBoard = deepcopy(board)
+                newBoard.make_move(*move)
+                eval = self.alphaBeta(board, depth - 1, alpha, beta, True)
+                minEval = min(minEval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+            print(minEval)
+            return minEval    
+            
+    
     def minimax(self, board, depth, maxPlayer):
         # Recursively calculate the best move using the minimax algorithm to the specified depth (assuming depth is a positive integer)
         #If we have reached the specified depth, and 
@@ -61,9 +92,20 @@ class nguzzone_ChessPlayer(ChessPlayer):
                     bestBoard = deepcopy(new_board)
             return minEval
         
-    def findBestMove(self, board):
+    def findBestMove(self, board, your_remaining_time, opp_remaining_time,):
+        print(your_remaining_time)
+        if your_remaining_time > opp_remaining_time + opp_remaining_time/ 6 and  500 > your_remaining_time > 400:
+            depth = 2
+        elif your_remaining_time > opp_remaining_time + opp_remaining_time/ 6 or 175 > your_remaining_time < 400:
+            depth = 3
+        elif your_remaining_time > opp_remaining_time and your_remaining_time < 500:
+            depth = 2
+        elif opp_remaining_time > your_remaining_time or your_remaining_time > 500 or your_remaining_time > 30:
+            depth = 1
+        else:
+            depth = 2
+        
       #  print("findBest")
-        depth = 2
         bestMove = None
         bestValue = float(-99999) if self.color == 'white' else float(99999)
         color = deepcopy(self.color)
@@ -72,7 +114,7 @@ class nguzzone_ChessPlayer(ChessPlayer):
             
             newBoard = deepcopy(board)
             newBoard.make_move(*move)
-            moveValue = self.minimax(newBoard, depth, self.color == 'white')
+            moveValue = self.alphaBeta(newBoard, depth, float(-99999), float(99999), self.color == 'white')
             if self.color == 'white' and moveValue > bestValue:
                 bestValue = moveValue
                 bestMove = move
@@ -83,14 +125,29 @@ class nguzzone_ChessPlayer(ChessPlayer):
         print(bestMove)
         return bestMove
     
-    
+
     #inish thests comments then opitminze this rundtion. 
-    
-    def evaluateBoard(self, board):
+
+
+    def evaluateBoard(self, board, depth):
         e = 0
         for pos, piece in board.items():
             
             val = 0
+            color = self.color
+            if self.color == 'black' and board.is_king_in_checkmate(color):
+                val = 99999
+                return val
+            elif self.color == 'black' and board.is_king_in_checkmate('white'):
+                val = -99999
+                return val
+            elif self.color == 'white' and board.is_king_in_checkmate(color):
+                val = -99999
+                return val
+            elif self.color == 'white' and board.is_king_in_checkmate('black'):
+                val = 99999
+                return val
+            
             
             if piece.get_notation() == 'K':
                 val = 900
@@ -126,4 +183,4 @@ class nguzzone_ChessPlayer(ChessPlayer):
             
         return e
     
-            
+         
